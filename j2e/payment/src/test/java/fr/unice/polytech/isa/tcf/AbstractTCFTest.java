7a327d8001fc5e4cc27b4d12d306d508298698b9
Dorian Blanc <Dorian Blanc>
2016-04-07 12:31:20 +0200

    découpage en branches

---
+++ j2e/payment/src/test/java/fr/unice/polytech/isa/tcf/AbstractTCFTest.java
@@ -0,0 +1,43 @@
+package fr.unice.polytech.isa.tcf;
+
+import fr.unice.polytech.isa.tcf.components.CashierBean;
+import fr.unice.polytech.isa.tcf.components.KitchenBean;
+import fr.unice.polytech.isa.tcf.entities.Customer;
+import fr.unice.polytech.isa.tcf.exceptions.AlreadyExistingCustomerException;
+import fr.unice.polytech.isa.tcf.interceptors.Logger;
+import fr.unice.polytech.isa.tcf.utils.Database;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+import javax.ejb.EJB;
+
+public abstract class AbstractTCFTest {
+	
+	@EJB
+	protected Database memory;
+
+	@Deployment
+	public static WebArchive createDeployment() {
+		// Building a Web ARchive (WAR) containing the following elements:
+		return ShrinkWrap.create(WebArchive.class)
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+				// Utils
+				.addPackage(Database.class.getPackage())
+				// Entities
+				.addPackage(Customer.class.getPackage())
+				// Entities
+				.addPackage(OrderProcessing.class.getPackage())
+				// Entities
+				.addPackage(CashierBean.class.getPackage())
+				// Entities
+				.addPackage(KitchenBean.class.getPackage())
+				.addPackage(Payment.class.getPackage())
+				// Interceptors
+				.addPackage(Logger.class.getPackage())
+				// Exceptions
+				.addPackage(AlreadyExistingCustomerException.class.getPackage());
+	}
+
+}
