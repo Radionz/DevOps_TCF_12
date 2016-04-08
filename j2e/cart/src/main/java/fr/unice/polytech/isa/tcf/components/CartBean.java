b0a1179569e796086a213814e0ce359ba1f4967d
ThibautSoriano <st405262@etu.unice.fr>
2016-04-08 09:52:37 +0200

    Update CartBean.java

--- j2e/cart/src/main/java/fr/unice/polytech/isa/tcf/components/CartBean.java
+++ j2e/cart/src/main/java/fr/unice/polytech/isa/tcf/components/CartBean.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public abstract class CartBean implements CartModifier {
 
 	@EJB
-	protected Payment cashier;
+	protected Payment cashieeer;
 
 	@Override
 	@Interceptors({CartCounter.class})
